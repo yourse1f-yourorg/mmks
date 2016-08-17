61d4923996c04d3cb5caf80fe3174c03da6cc5a7
yourse1f-yourorg <doowa.diddee@gmail.com>
2016-08-17 08:33:01 -0400

    feat(cd): Let user know that the app has passed all setup procedures and is starting to run

--- server/main.js
+++ server/main.js
@@ -5,19 +5,23 @@ import { initPosts, initAccessPoints } from './configs/initial_adds.js';
 
 Meteor.startup(function () {
 
+/* eslint-disable no-console   */
+  const msg = '**** [Build and Deploy : Successful] :: ' +
+              'Meteor Mantra Kickstarter is starting now . . . ';
   if ( process.env.LOGGLY_TOKEN &&
        process.env.LOGGLY_TOKEN.length &&
        process.env.LOGGLY_TOKEN.length === 36 ) {
+    console.log(msg);
     return;
   }
 
   if ( Meteor.settings.LOGGLY_TOKEN &&
        Meteor.settings.LOGGLY_TOKEN.length &&
        Meteor.settings.LOGGLY_TOKEN.length === 36 ) {
+    console.log(msg);
     return;
   }
 
-/* eslint-disable no-console   */
   console.log('\n\n **STOPPED **\n' +
     '     Please ensure that your execution environment or the file, "settings.json", ' +
                              'has a valid Loggly authentication token named LOGGLY_TOKEN.\n' +
