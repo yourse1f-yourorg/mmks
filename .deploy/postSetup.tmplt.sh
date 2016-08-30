aa2ceb8fbf20b24addb03f96f820d3ccbecb413a
yourse1f-yourorg <doowa.diddee@gmail.com>
2016-08-30 08:18:16 -0400

    bug(cd): Track down permssions problem of project configuration directory.

--- .deploy/postSetup.tmplt.sh
+++ .deploy/postSetup.tmplt.sh
@@ -3,9 +3,16 @@
 echo "IN  : >>>>>>>>>>>>>>> Setting up private elements <<<<<<<<<<<<<<<";
 
 MUP_CONFIG_DIR="/opt/${REPO_PROJECT_NAME}/config";
+
+rm -fr ${MUP_CONFIG_DIR};        #  In case /opt/${REPO_PROJECT_NAME} gets cached 
 sudo mkdir -p ${MUP_CONFIG_DIR};
 
-cp /home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}_bundle.crt ${MUP_CONFIG_DIR}/bundle.crt
-openssl rsa -passin file:/home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}.pkp -in /home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}.key -out ${MUP_CONFIG_DIR}/private.key 
+cp /home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}_bundle.crt \
+                                  ${MUP_CONFIG_DIR}/bundle.crt;
+
+openssl rsa -passin \
+    file:/home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}.pkp \
+     -in /home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}.key \
+     -out ${MUP_CONFIG_DIR}/private.key;
 
 echo "OUT : >>>>>>>>>>>>>>>   Set up private elements   <<<<<<<<<<<<<<<";
