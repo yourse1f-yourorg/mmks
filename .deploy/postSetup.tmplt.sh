cfdac08a0bd596a09bdcc38dee5dd3bf33332fbc
yourse1f-yourorg <doowa.diddee@gmail.com>
2016-08-30 10:26:49 -0400

    bug(cd): Track down mup ssl config dir permssions problem.

--- .deploy/postSetup.tmplt.sh
+++ .deploy/postSetup.tmplt.sh
@@ -2,11 +2,17 @@
 #
 echo "IN  : >>>>>>>>>>>>>>> Setting up private elements <<<<<<<<<<<<<<<";
 
-MUP_CONFIG_DIR="/opt/${REPO_PROJECT_NAME}/config";
+# Reference note : https://github.com/meteorhacks/mup-frontend-server
+
+MUP_STAGING_DIR="/opt/${REPO_PROJECT_NAME}";
+MUP_CONFIG_DIR="${MUP_STAGING_DIR}/config";
 
 echo "recreate dir : ${MUP_CONFIG_DIR}";
 rm -fr ${MUP_CONFIG_DIR};        #  In case /opt/${REPO_PROJECT_NAME} gets cached 
 sudo mkdir -p ${MUP_CONFIG_DIR};
+sudo chown -R ${USER}:${USER} ${MUP_STAGING_DIR};
+ls -l ${MUP_STAGING_DIR}/..;
+ls -l ${MUP_STAGING_DIR};
 
 echo "cp /home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}_bundle.crt ${MUP_CONFIG_DIR}/bundle.crt;";
 cp /home/${HOST_USER_NAME}/.ssh/${HOST_SERVER_NAME}_bundle.crt \
