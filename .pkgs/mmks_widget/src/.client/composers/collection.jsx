4fd3c91cf9bd57988c9de18cf53a23326d8fae14
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2016-11-26 13:23:34 -0500

    enhancement(plugins): Can build Node packages containing JSX files

--- .pkgs/mmks_widget/src/.client/composers/collection.jsx
+++ .pkgs/mmks_widget/src/.client/composers/collection.jsx
@@ -1,6 +1,5 @@
 import {useDeps} from 'react-simple-di';
-import {composeAll} from 'react-komposer';
-import composeWithTracker from '../../../../lib/compose-with-tracker.js';
+import { composeAll, composeWithTracker } from 'mantra-core';
 
 export const collectionComposer = ({context}, onData) => {
 
