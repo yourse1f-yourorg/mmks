e557227c50c566c9340a3253a79448bcd62aa5f5
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2016-11-30 11:52:21 -0500

    Admin (linting):  Residual debug logging

--- client/modules/_colors/containers/add.jsx
+++ client/modules/_colors/containers/add.jsx
@@ -6,7 +6,6 @@ import AcCont from '/client/access_control/acContainer.js';
 
 import dataComposer from '../composers/add.jsx';
 import Component from '../components/_form.jsx';
-// const Container = dataComposer(Component);
 
 let Container = function dummy() { return <div></div>; };
 let Authorized = function dummy() { return <div></div>; };
@@ -21,13 +20,8 @@ export default class extends React.Component {
 
   render() {
 
-    console.log('......... client/container/add .............');
-    console.log(Container);
-    console.log('.................>  <....................');
-
     const apAdd = {module: 'colors', action: 'add'};
     const accPnts = [ apAdd ];
-//    console.log(' ad cont - -   apAdd : ', apAdd);
 
     return (
       <div className="bs-docs-section clearfix">
@@ -46,9 +40,3 @@ export default class extends React.Component {
 
   }
 }
-
-/*
-            <Authorized accesspoints={accPnts} warn='true'>
-              <Container />
-            </Authorized>
-*/
