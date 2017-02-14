1eefce2b8e3a8ffb8c53d8c7575e303eae1a09c0
Yourself <yourself.yourorg@gmail.com>
2017-02-14 18:23:48 -0500

    Admin (junk): Useless __template module eliminated

--- client/modules/__template/actions/index.js
+++
@@ -1,5 +0,0 @@
-// import _model from './posts';
-
-export default {
-  // _model
-};
--- client/modules/__template/configs/method_stubs/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-// import posts from './posts';
-
-export default function (context) {
-  // posts(context);
-}
