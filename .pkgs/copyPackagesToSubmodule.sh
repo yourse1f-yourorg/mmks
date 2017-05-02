f006af7c6d8ad0732e22059f682612f49f4e4a93
You Yourself <yourse1f-yourorg@gmail.com>
2017-05-02 17:19:09 -0400

    Enhancement (git module): Adds exclusion lists for packages of submodule and container repo
    Signed-off-by: You Yourself <yourse1f-yourorg@gmail.com>

--- .pkgs/copyPackagesToSubmodule.sh
+++ .pkgs/copyPackagesToSubmodule.sh
@@ -2,27 +2,53 @@
 #
 pushd `dirname $0` > /dev/null; SCRIPTPATH=`pwd`; popd > /dev/null;
 
+
+declare PKG_EXCLUSIONS="package_exclusions.json";
+declare PKG_EXCL_PATH="./${PKG_EXCLUSIONS}";
+
+declare GITIG_PREFIX="gitignored_";
+
 function copyPackagesToSubmodule() {
   local CONTAINER_PKGS_DIR=$1;
   local SUBMODULE_PKGS_DIR=$2;
-  echo -e "### Identifying available npm packages for user app";
+  echo -e "### Identifying available npm packages for user app.  ${SCRIPTPATH}";
+  echo -e "### Will copy from '${CONTAINER_PKGS_DIR}' to '${SUBMODULE_PKGS_DIR}'";
+
   pushd ${CONTAINER_PKGS_DIR} >/dev/null;
 
-    for PKGE in ./*/
+    declare EXCLUSIONS=$(jq -r .packages_excluded_from_wrapper ${PKG_EXCL_PATH});
+    echo -e "Exclusions ...\n${EXCLUSIONS}";
+    for MODULE_PATH in ./*/
     do
-      if touch ${PKGE}package.json 2>/dev/null; then
-        if [[ "X$(cat ${PKGE}package.json  | jq -r .name)X" != "XX" ]]; then
-          echo -e "~~~~~~~~~~  Copy '${PKGE}' into submodule ~~~~~~~~~~~~~~~~~~~~";
-          cp -r ${PKGE} ${SUBMODULE_PKGS_DIR}/gitignored_$(basename ${PKGE});
+      # echo -e "###  ${MODULE_PATH}";
+      NODE_META=${MODULE_PATH}package.json;
+      # echo ${NODE_META};
+      if [[ -r ${NODE_META} && -f ${NODE_META} ]]; then
+        MDL=$(cat ${NODE_META}  | jq -r .name);
+        # echo -e "MDL = ${MDL}";
+        if [[ "X${MDL}X" != "XX" ]]; then
+          EXCLUDE=$(echo ${EXCLUSIONS} | jq ". | contains([\"${MDL}\"])");
+          echo -e "~~~~~~~~~~  Exclude '${MDL}' from project? ${EXCLUDE} ~~~~~~~~~~~";
+          if [[ "true" != "${EXCLUDE}" ]]; then
+            echo -e " Copying '${MDL}' to project .pkgs/${GITIG_PREFIX}$(basename ${MODULE_PATH})  ~~~~~~~~~~~";
+
+            # echo "${MDL} to list >>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+            # echo "${MDL}" >> ${LOCAL_NODEJS_ PACKAGES_LIST};                DEPRECATED ????
+
+            cp -r ${MODULE_PATH} ${SUBMODULE_PKGS_DIR}/${GITIG_PREFIX}$(basename ${MODULE_PATH});
+
+          fi;
         fi;
       fi;
-    done
-#    echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
-  popd >/dev/null;
+     done
+   echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+   popd >/dev/null;
 
 }
 
-# if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-#   copyPackagesToSubmodule ${SCRIPTPATH} ;
-# fi;
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  SUB_MDL_PKG_DIR=${SCRIPTPATH}/../meteor-mantra-kickstarter/.pkgs
+  copyPackagesToSubmodule ${SCRIPTPATH} ${SUB_MDL_PKG_DIR};
+fi;
--- clean_all.sh
+++ clean_all.sh
@@ -3,5 +3,8 @@
 source ./meteor-mantra-kickstarter/clean_all.sh;
 
 pushd ./meteor-mantra-kickstarter >/dev/null;
+  rm -fr .pkgs/gitignored_*;
   CleanAllInstalledPackages;
 popd >/dev/null;
+
+rm -fr ~/.meteor/packages/meteor-tool/1.4.4_1/mt-os.linux.x86_64/dev_bundle/lib/node_modules/mmks_*;
