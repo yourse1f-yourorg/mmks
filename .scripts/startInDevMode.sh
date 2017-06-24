7d970b25215c369d7da5e2b686190195917df670
Martin H. Bramwell <martinhbramwell@gmail.com>
2017-06-24 08:33:11 -0400

    Enhance (modules) : Allow 'yes/no' value for IS_GITSUBMODULE
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- .scripts/startInDevMode.sh
+++ .scripts/startInDevMode.sh
@@ -2,7 +2,7 @@
 #
 pushd `dirname $0` > /dev/null; SCRIPTPATH=`pwd`; popd > /dev/null;
 PROJECT_ROOT=${SCRIPTPATH%/.scripts};
-export IS_GITSUBMODULE="true"
+
 pushd ${PROJECT_ROOT}/meteor-mantra-kickstarter >/dev/null;
   .scripts/startInDevMode.sh $1;
 popd >/dev/null;
