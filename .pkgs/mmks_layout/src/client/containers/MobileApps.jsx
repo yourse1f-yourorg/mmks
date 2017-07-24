20e576d99dd3245b669f8050264dba408a24ea85
Martin H. Bramwell <martinhbramwell@gmail.com>
2017-07-24 15:34:45 -0400

    Bug (mobile): Correct acces path to APK download
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- .pkgs/mmks_layout/src/client/containers/MobileApps.jsx
+++ .pkgs/mmks_layout/src/client/containers/MobileApps.jsx
@@ -5,7 +5,7 @@ export default class extends React.Component {
   render() {
     return (
         <a aria-expanded="false" role="button"
-          href="/mobile/android/mmks.apk" download>
+          href="/public/mobile/android/mmks.apk" download>
            <img src="/mobile/android/Android_robot.svg"
                 alt="Android APK" width="22" height="22" />
         </a>
