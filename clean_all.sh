c21e02d0fd758c5c99b58595f58f2d5c6db38945
Martin H. Bramwell <martinhbramwell@gmail.com>
2017-08-28 20:39:08 -0400

    Bug (bld): Must pass parameter through to lower layer
    Signed-off-by: Martin H. Bramwell <martinhbramwell@gmail.com>

--- clean_all.sh
+++ clean_all.sh
@@ -3,6 +3,6 @@
 source ./meteor-mantra-kickstarter/clean_all.sh;
 
 pushd ./meteor-mantra-kickstarter >/dev/null;
-  CleanAllInstalledPackages;
+  CleanAllInstalledPackages ${1};
   sed -i '/^ *\"mmks[a-zA-Z0-9_]*\": *\"/d' package.json;
 popd >/dev/null;
